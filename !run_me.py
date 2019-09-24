1243318d2c64183876790152d076cb53d5a34940
Олико Человик <kaper156oi@gmail.com>
2019-09-24 20:06:10 +0600

    Refactoring in sel_new: move methods in one class - BrowserScenario;

--- !run_me.py
+++ !run_me.py
@@ -1,17 +1,15 @@
 from check_csv_and_url import get_undownloaded_urls
 from compile import csv_out_gen_sum, csv_out_gen_increment
-from sel_new import manage_weeks
+from sel_new import BrowserScenario
 from settings import *
 from url_generator import generate_urls
 
 if __name__ == '__main__':
     # Генерирует ссылки в файл urls.txt
-    generate_urls(
-        BASE_URL=BASE_URL
-        , FROM_DATE=FROM_DATE
-        , TO_DATE=TO_DATE
-        , DATE_TYPE=DATE_TYPE
-    )
+    generate_urls(BASE_URL=BASE_URL,
+                  FROM_DATE=FROM_DATE, TO_DATE=TO_DATE,
+                  DATE_TYPE=DATE_TYPE
+                  )
 
     # Читает ссылки из файла urls.txt
     with open("urls.txt", "rt") as f:
@@ -20,12 +18,16 @@ if __name__ == '__main__':
     # Создаем папку для загурзки
     os.mkdir(FOLDER_NAME)
 
-    while len(urls):
-        manage_weeks(urls, FOLDER_NAME, is_authorization_needed)
-        urls = get_undownloaded_urls(urls, FOLDER_NAME)
-        is_authorization_needed = False
-        break
-    exit()
+    # Работаем с браузером
+    with BrowserScenario(FOLDER_NAME, PROFILE_PATH, IS_AUTH_NEEDED) as bs:
+        # Пока остаются ссылки
+        while len(urls):
+            for url in urls:
+                bs.get_week_data(url)
+            # Собираем пропущенные ссылки
+            urls = get_undownloaded_urls(urls, FOLDER_NAME)
+            bs.is_authorization_needed = False
+
     if AVG_CSV:
         csv_out_gen_sum(FOLDER_NAME, FILTERS)
     else:
--- sel_new.py
+++ sel_new.py
@@ -10,49 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-browser = None
-cookie_keeper = None
-
-PROFILE_PATH = " "
-
-
-def set_profile(path):
-    global PROFILE_PATH
-    PROFILE_PATH = path
-
-
-def run_browser(download_dir, is_authorization_needed):
-    global browser, cookie_keeper
-    fp = webdriver.FirefoxProfile(PROFILE_PATH)
-    fp.set_preference('browser.download.folderList', 2)
-    fp.set_preference('browser.download.manager.showWhenStarting', False)
-    fp.set_preference('browser.download.dir', download_dir)
-    fp.set_preference('browser.download.useDownloadDir', True)
-    fp.set_preference('browser.helperApps.neverAsk.saveToDisk',
-                      'text/plain, application/vnd.ms-excel, text/csv, text/comma-separated-values, '
-                      'application/octet-stream, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    fp.update_preferences()
-
-    caps = DesiredCapabilities.FIREFOX.copy()
-    caps["firefox_profile"] = fp.encoded
-    caps["marionette"] = True
-    browser = webdriver.Firefox(firefox_profile=fp, firefox_binary="C:/Program Files/Mozilla Firefox/firefox.exe",
-                                capabilities=caps,
-                                executable_path='../selenium_drv/geckodriver.exe')
-    # browser = webdriver.Firefox(executable_path='../selenium_drv/geckodriver.exe')
-    browser.implicitly_wait(10)
-    browser.set_script_timeout(10)
-
-    cookie_keeper = CookieKeeper(browser)
-    if is_authorization_needed:
-        browser.get("https://google.ru")
-        cookie_keeper.load_cookies()
-        browser.get("https://accounts.google.com/signin/v2")
-        input("Войдите на сайт и нажмите enter (Возможно куки уже загрузились, попробуйте перезагрузить страницу)")
-        cookie_keeper.save_cookies()
-    cookie_keeper.load_cookies()
-
-
+# css selectors
 ANCHOR_LOADED = 'th.ACTION-sort'
 ANCHOR_NEXT = "li.ACTION-paginate:nth-of-type(2)"
 
@@ -70,94 +28,125 @@ class CookieKeeper:
             pickle.dump(self.__browser__.get_cookies(), file)
 
     def load_cookies(self):
-        with open(self.__file_path__, 'rb') as file:
-            cookies = pickle.load(file)
-            for cookie in cookies:
-                self.__browser__.add_cookie(cookie)
-
-
-def wait_for(target_css, timeout=15):
-    wait = WebDriverWait(browser, timeout)
-    return wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, target_css)))
-
-
-def click_and_wait(target_css, waited_for_css, timeout=7):
-    wait = WebDriverWait(browser, timeout)
-    # wait.until_not(EC.presence_of_element_located((By.CLASS_NAME, "ID-loadingProgressBarScreen")))
-    while True:
         try:
-            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, target_css))).click()
-        except (ElementClickInterceptedException):
-            time.sleep(1)
-        else:
-            break
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, waited_for_css)))
+            with open(self.__file_path__, 'rb') as file:
+                cookies = pickle.load(file)
+                for cookie in cookies:
+                    self.__browser__.add_cookie(cookie)
+        except EOFError:
+            print("Cookie не найдены")
 
 
-def get_page_data(count=None):
-    try:
-        click_and_wait(".ID-exportControlButton", "li.ACTION-export.TARGET-CSV")
-        click_and_wait("li.ACTION-export.TARGET-CSV", ANCHOR_LOADED)
-    except (TimeoutException, StaleElementReferenceException, ElementNotInteractableException):
-        if count is None:
-            count = 0
+class BrowserScenario:
+    __FIREFOX_BINARY_PATH__ = "C:/Program Files/Mozilla Firefox/firefox.exe"
+    __GECKO_DRIVER_PATH__ = '../selenium_drv/geckodriver.exe'
 
-        if count >= 2:
-            print("Something is wrong third try save csv")
-            time.sleep(5)
-        if count >= 3:
-            input()
+    def __init__(self, download_dir, profile_path: str, is_authorization_needed=True):
+        self.download_dir = download_dir
+        self.is_authorization_needed = is_authorization_needed
+        self.profile_path = profile_path
 
-        count += 1
-        get_page_data(count=count)
+        self.browser = None
+        self.cookie = None
 
+    def run_browser(self):
+        fp = webdriver.FirefoxProfile(self.profile_path)
+        fp.set_preference('browser.download.folderList', 2)
+        fp.set_preference('browser.download.manager.showWhenStarting', False)
+        fp.set_preference('browser.download.dir', self.download_dir)
+        fp.set_preference('browser.download.useDownloadDir', True)
+        fp.set_preference('browser.helperApps.neverAsk.saveToDisk',
+                          'text/plain, application/vnd.ms-excel, text/csv, text/comma-separated-values, '
+                          'application/octet-stream, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        fp.update_preferences()
 
-def browse_next_page():
-    pagination_text = browser.find_element_by_css_selector("span.C_PAGINATION_ROWS_LONG>label").text
-    pagination_text = pagination_text.split(" ")
-    if pagination_text[2] == pagination_text[4]:
+        caps = DesiredCapabilities.FIREFOX.copy()
+        caps["firefox_profile"] = fp.encoded
+        caps["marionette"] = True
+        browser = webdriver.Firefox(firefox_profile=fp, firefox_binary=self.__FIREFOX_BINARY_PATH__,
+                                    capabilities=caps,
+                                    executable_path=self.__GECKO_DRIVER_PATH__)
+        browser.implicitly_wait(10)
+        browser.set_script_timeout(10)
+
+        self.cookie = CookieKeeper(browser)
+        if self.is_authorization_needed:
+            browser.get("https://google.ru")
+            self.cookie.load_cookies()
+            browser.get("https://accounts.google.com/signin/v2")
+            input("Войдите на сайт и нажмите enter (Возможно куки уже загрузились, попробуйте перезагрузить страницу)")
+            self.cookie.save_cookies()
+        self.cookie.load_cookies()
+
+    def __wait_for(self, target_css, timeout=15):
+        wait = WebDriverWait(self.browser, timeout)
+        return wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, target_css)))
+
+    def __click_and_wait(self, target_css, waited_for_css, timeout=7):
+        wait = WebDriverWait(self.browser, timeout)
+        # wait.until_not(EC.presence_of_element_located((By.CLASS_NAME, "ID-loadingProgressBarScreen")))
+        while True:
+            try:
+                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, target_css))).click()
+            except (ElementClickInterceptedException):
+                time.sleep(1)
+            else:
+                break
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, waited_for_css)))
+
+    def __wait_while_visible(self, _id, timeout=15):
+        try:
+            wait = WebDriverWait(self.browser, timeout)
+            wait.until_not(EC.visibility_of_element_located((By.ID, _id)))
+        except TimeoutException:
+            pass
+
+    def __get_page_data(self, count=None):
+        try:
+            self.__click_and_wait(".ID-exportControlButton", "li.ACTION-export.TARGET-CSV")
+            self.__click_and_wait("li.ACTION-export.TARGET-CSV", ANCHOR_LOADED)
+        except (TimeoutException, StaleElementReferenceException, ElementNotInteractableException):
+            # TODO refactoring method
+            if count is None:
+                count = 0
+
+            if count >= 2:
+                print("Something is wrong third try save csv")
+                time.sleep(5)
+            if count >= 3:
+                input()
+
+            count += 1
+            self.__get_page_data(count=count)
+
+    def __browse_next_page(self):
+        pagination_text = self.browser.find_element_by_css_selector("span.C_PAGINATION_ROWS_LONG>label").text
+        pagination_text = pagination_text.split(" ")
+        if pagination_text[2] == pagination_text[4]:
+            return False
+        self.__click_and_wait(ANCHOR_NEXT, ANCHOR_LOADED, 15)
+
+    def get_week_data(self, url):
+        self.browser.get(url)
+        while True:
+            self.__wait_for("iframe#galaxyIframe")
+            self.browser.switch_to_frame(self.browser.find_element_by_id("galaxyIframe"))
+            self.__wait_while_visible("ID-reportLoading")
+            self.__get_page_data()
+            self.__wait_while_visible("ID-messageBox")
+            if not self.__browse_next_page():
+                break
+
+    def __enter__(self):
+        # open browser
+        self.run_browser()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # close browser
+        self.browser.close()
         return False
-    click_and_wait(ANCHOR_NEXT, ANCHOR_LOADED, 15)
-
-
-def wait_while_visible(_id, timeout=15):
-    try:
-        wait = WebDriverWait(browser, timeout)
-        wait.until_not(EC.visibility_of_element_located((By.ID, _id)))
-    except TimeoutException:
-        pass
-
-
-def get_week_data(url):
-    browser.get(url)
-    while True:
-        wait_for("iframe#galaxyIframe")
-        browser.switch_to_frame(browser.find_element_by_id("galaxyIframe"))
-        wait_while_visible("ID-reportLoading")
-        get_page_data()
-        wait_while_visible("ID-messageBox")
-        if not browse_next_page():
-            break
-
-
-def manage_weeks(urls, download_dir, is_authorization_needed=False):
-    run_browser(download_dir, is_authorization_needed)
-    for url in urls:
-        get_week_data(url)
-    browser.close()
-
-
-def wait_cookies(download_dir):
-    run_browser(download_dir, True)
-    input("Нажмите enter, после авторизации")
-    cookie_keeper.save_cookies()
-    browser.close()
 
 
 if __name__ == '__main__':
-    # log_in__google("analytics@delvepartners.com", "R]q(0Q7F?RvCF\-J")
-    # save_cookies()
-    # exit()
-    browser.get("https://analytics.google.com")
-    cookie_keeper.load_cookies()
-    # manage_weeks()
+    pass
